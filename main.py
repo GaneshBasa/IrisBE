a35593a65030d879110514a0650f877b3b4364ed
Ganesh Basa <basaganesh1@gmail.com>
2024-06-08 16:19:36 +0530

    HTTPS Redirect

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from app.config import ORIGINS, ORIGIN_REGEX
 
 app = FastAPI()
 
-app.add_middleware( CORSMiddleware, allow_origins=ORIGINS, allow_origin_regex=ORIGIN_REGEX, allow_credentials=True, allow_methods=['*'], allow_headers=['*'] )
+app.add_middleware( HTTPSRedirectMiddleware )
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=ORIGINS,
+  allow_origin_regex=ORIGIN_REGEX,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
 
 @app.get( '/' )
 async def root():
