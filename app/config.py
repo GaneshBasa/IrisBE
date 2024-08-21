d7a3ef36a7f1a928a0f1505a5a0c2ec6d08b8992
Ganesh Basa <basaganesh1@gmail.com>
2024-08-21 23:20:10 +0530

    Added Middleware

--- app/config.py
+++ app/config.py
@@ -5,4 +5,9 @@ from app.helpers import get_env_var
 if ORIGINS := get_env_var( 'ORIGINS' ):
   ORIGINS = loads( ORIGINS )
 else:
-  ORIGINS = [ 'http://localhost:3000' ]
+  ORIGINS = [ '*' ]
+
+if ALLOWED_HOSTS := get_env_var( 'ALLOWED_HOSTS' ):
+  ALLOWED_HOSTS = loads( ALLOWED_HOSTS )
+else:
+  ALLOWED_HOSTS = [ '*' ]
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-from app.config import ORIGINS
+from app.config import ORIGINS, ALLOWED_HOSTS
 
 
 app = FastAPI()
 
 
 app.add_middleware( HTTPSRedirectMiddleware )
+
+app.add_middleware( GZipMiddleware, compresslevel=5 )
+
+app.add_middleware( TrustedHostMiddleware, allowed_hosts=ALLOWED_HOSTS )
 
 app.add_middleware(
   CORSMiddleware,
