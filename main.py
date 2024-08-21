204f57090d623f013e4412f469a5c62abfdd946f
Ganesh Basa <basaganesh1@gmail.com>
2024-08-21 23:16:03 +0530

    Added GZip Middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
@@ -11,10 +12,9 @@ app = FastAPI()
 
 app.add_middleware( HTTPSRedirectMiddleware )
 
-app.add_middleware(
-  TrustedHostMiddleware,
-  allowed_hosts=ALLOWED_HOSTS
-)
+app.add_middleware( GZipMiddleware, compresslevel=5 )
+
+app.add_middleware( TrustedHostMiddleware, allowed_hosts=ALLOWED_HOSTS )
 
 app.add_middleware(
   CORSMiddleware,
