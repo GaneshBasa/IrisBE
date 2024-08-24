a208bad25031c8d2041c619a69ea911c6d9cdfa4
Ganesh Basa <basaganesh1@gmail.com>
2024-08-24 06:04:17 +0530

    WS Test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from asyncio import sleep
 
 from app.config import ORIGINS, ALLOWED_HOSTS
 
@@ -35,6 +36,21 @@ async def ws_root( ws: WebSocket ):
   try:
     await ws.accept()
     await ws.send_json( { 'ws_msg': 'Hello from Iris App FastAPI Server' } )
+    await ws.close()
+  
+  except WebSocketDisconnect:
+    pass
+
+
+@app.websocket( '/test' )
+async def test( ws: WebSocket ):
+  try:
+    await ws.accept()
+    
+    for i in range( 10 ):
+      await ws.send_json( { 'count': i + 1 } )
+      await sleep( 1 )
+    
     await ws.close()
   
   except WebSocketDisconnect:
